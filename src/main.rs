2da2c334b1e4afa183fa7bbb845460697f4d2f22
Luiz Rocco <luizgustavorocco@gmail.com>
2023-03-24 00:23:06 -0300

    Added tests to project and expect new fns to also have tests from now on

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ fn contains_duplicate(nums: Vec<i32>) -> bool {
     nums.len() != HashSet::<i32>::from_iter(nums).len()
 }
 
+#[test]
+fn test_contains_subarray() {
+    assert_eq!(contains_duplicate(vec![1, 2, 3, 1]), true);
+    assert_eq!(contains_duplicate(vec![1, 2, 3, 4]), false);
+    assert_eq!(contains_duplicate(vec![1, 1, 1, 3, 3, 4, 3, 2, 4, 2]), true);
+}
+
 fn max_sub_array(nums: Vec<i32>) -> i32 {
     let mut result = i32::MIN;
     let mut acc = 0;
@@ -26,6 +33,13 @@ fn max_sub_array(nums: Vec<i32>) -> i32 {
     result
 }
 
+#[test]
+fn test_max_sub_array() {
+    assert_eq!(max_sub_array(vec![5, 4, -1, 7, 8]), 23);
+    assert_eq!(max_sub_array(vec![1]), 1);
+    assert_eq!(max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
+}
+
 fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut hash = HashMap::<i32, usize>::new();
     for (index, &el) in nums.iter().enumerate() {
@@ -38,12 +52,11 @@ fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     vec![]
 }
 
-fn quick_sort<T: PartialOrd>(nums: &mut [T]) -> &[i32] {
-    let length = nums.len();
-
-    nums.swap(length - 1, length / 2);
-
-    let left_part = todo!();
+#[test]
+fn test_two_sum() {
+    assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+    assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+    assert_eq!(two_sum(vec![3, 3], 6), vec![0, 1]);
 }
 
 fn binary_search(nums: &mut [i32], target: i32) -> Option<usize> {
@@ -63,6 +76,43 @@ fn binary_search(nums: &mut [i32], target: i32) -> Option<usize> {
     None
 }
 
+#[test]
+fn test_binary_search() {
+    assert_eq!(
+        binary_search(&mut [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9),
+        Some(8)
+    );
+    assert_eq!(
+        binary_search(&mut [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1),
+        Some(0)
+    );
+    assert_eq!(
+        binary_search(&mut [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 15),
+        None
+    );
+}
+
+fn quick_sort<T: PartialOrd>(nums: &mut [T]) {
+    fn partition<T: PartialOrd>(nums: &mut [T], low: usize, high: usize) -> usize {
+        todo!()
+    }
+
+    fn _quick_sort<T: PartialOrd>(nums: &mut [T], low: usize, high: usize) {
+        if (low < high) {
+            let pivot_index = partition(nums, low, high);
+            _quick_sort(nums, low, pivot_index);
+            _quick_sort(nums, pivot_index, high);
+        }
+    }
+
+    _quick_sort(nums, 0, nums.len());
+}
+
+// #[test]
+// fn test_quick_sort() {
+//     assert_eq!()
+// }
+
 fn bubble_sort() {
     todo!();
 }
@@ -73,10 +123,4 @@ fn insertion_sort() {
 
 fn selection_sort() {
     todo!();
-}
-
-fn main() {
-    let mut arr = vec![1, 2, 3, 4, 6, 7, 11, 14];
-    let result = binary_search(&mut arr, 1).unwrap();
-    println!("your number index is: {:?}", result);
 }
