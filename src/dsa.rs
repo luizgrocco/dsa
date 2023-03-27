43cfa846f44bb02849f3ec14565b4f04c7f02819
Luiz Rocco <luizgustavorocco@gmail.com>
2023-03-26 21:41:49 -0300

    Finished implemeting all the BASIC n**2 sorting algorithms

--- src/dsa.rs
+++ src/dsa.rs
@@ -1,22 +1,24 @@
 #![allow(unused)]
 use std::collections::{HashMap, HashSet};
+use std::fmt::Debug;
+use std::hash::Hash;
 
-fn contains_duplicate(nums: Vec<i32>) -> bool {
-    nums.len() != HashSet::<i32>::from_iter(nums).len()
+fn contains_duplicate<T: Eq + Hash>(nums: &[T]) -> bool {
+    nums.len() != HashSet::<&T>::from_iter(nums).len()
 }
 
 #[test]
-fn test_contains_subarray() {
-    assert_eq!(contains_duplicate(vec![1, 2, 3, 1]), true);
-    assert_eq!(contains_duplicate(vec![1, 2, 3, 4]), false);
-    assert_eq!(contains_duplicate(vec![1, 1, 1, 3, 3, 4, 3, 2, 4, 2]), true);
+fn test_contains_duplicate() {
+    assert_eq!(contains_duplicate(&[1, 2, 3, 1]), true);
+    assert_eq!(contains_duplicate(&[1, 2, 3, 4]), false);
+    assert_eq!(contains_duplicate(&[1, 1, 1, 3, 3, 4, 3, 2, 4, 2]), true);
 }
 
-fn max_sub_array(nums: Vec<i32>) -> i32 {
-    let mut result = i32::MIN;
+fn max_sub_array(nums: &[i32]) -> Option<i32> {
+    let mut result: Option<i32> = None;
     let mut acc = 0;
 
-    for el in nums {
+    for &el in nums {
         let partial_sum = acc + el;
         if el > partial_sum {
             acc = el;
@@ -26,8 +28,13 @@ fn max_sub_array(nums: Vec<i32>) -> i32 {
             acc = partial_sum;
         }
 
-        if acc > result {
-            result = acc;
+        match result {
+            Some(valid_result) => {
+                if acc > valid_result {
+                    result = Some(acc);
+                }
+            }
+            None => result = Some(acc),
         }
     }
     result
@@ -35,31 +42,32 @@ fn max_sub_array(nums: Vec<i32>) -> i32 {
 
 #[test]
 fn test_max_sub_array() {
-    assert_eq!(max_sub_array(vec![5, 4, -1, 7, 8]), 23);
-    assert_eq!(max_sub_array(vec![1]), 1);
-    assert_eq!(max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
+    assert_eq!(max_sub_array(&[5, 4, -1, 7, 8]), Some(23));
+    assert_eq!(max_sub_array(&[1]), Some(1));
+    assert_eq!(max_sub_array(&[-2, 1, -3, 4, -1, 2, 1, -5, 4]), Some(6));
+    assert_eq!(max_sub_array(&[]), None);
 }
 
-fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+fn two_sum(nums: Vec<i32>, target: i32) -> Option<[i32; 2]> {
     let mut hash = HashMap::<i32, usize>::new();
     for (index, &el) in nums.iter().enumerate() {
         let diff = target - el;
         if let Some(&x) = hash.get(&diff) {
-            return vec![x as i32, index as i32];
+            return Some([x as i32, index as i32]);
         }
         hash.insert(el, index);
     }
-    vec![]
+    None
 }
 
 #[test]
 fn test_two_sum() {
-    assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
-    assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2]);
-    assert_eq!(two_sum(vec![3, 3], 6), vec![0, 1]);
+    assert_eq!(two_sum(vec![2, 7, 11, 15], 9).unwrap(), [0, 1]);
+    assert_eq!(two_sum(vec![3, 2, 4], 6).unwrap(), [1, 2]);
+    assert_eq!(two_sum(vec![3, 3], 6).unwrap(), [0, 1]);
 }
 
-fn binary_search(nums: &mut [i32], target: i32) -> Option<usize> {
+fn binary_search<T: PartialOrd>(nums: &mut [T], target: T) -> Option<usize> {
     let mut low = 0;
     let mut high = nums.len();
     while low < high {
@@ -92,33 +100,54 @@ fn test_binary_search() {
     );
 }
 
-fn quick_sort<T: PartialOrd>(nums: &mut [T]) {
-    fn partition<T: PartialOrd>(nums: &mut [T], low: usize, high: usize) -> usize {
-        todo!()
+fn bubble_sort<T: PartialOrd>(nums: &mut [T]) {
+    for i in 0..nums.len() {
+        for j in 0..nums.len() {
+            if j > 0 && nums[j] < nums[j - 1] {
+                nums.swap(j, j - 1);
+            }
+        }
     }
+}
 
-    fn _quick_sort<T: PartialOrd>(nums: &mut [T], low: usize, high: usize) {
-        if (low < high) {
-            let pivot_index = partition(nums, low, high);
-            _quick_sort(nums, low, pivot_index);
-            _quick_sort(nums, pivot_index, high);
+#[test]
+fn test_bubble_sort() {
+    let mut arr = [1, 7, 4, 3, 9, 2, 0];
+    bubble_sort(&mut arr);
+    assert_eq!(arr, [0, 1, 2, 3, 4, 7, 9]);
+    let mut arr = [1];
+    bubble_sort(&mut arr);
+    assert_eq!(arr, [1]);
+    let mut arr: [i32; 0] = [];
+    bubble_sort(&mut arr);
+    assert_eq!(arr, []);
+}
+
+fn insertion_sort<T: PartialOrd>(nums: &mut [T]) {
+    fn swap_until_smallest<T: PartialOrd>(nums: &mut [T]) {
+        for i in (0..nums.len()).rev() {
+            if i > 0 && nums[i] < nums[i - 1] {
+                nums.swap(i, i - 1);
+            }
         }
     }
 
-    _quick_sort(nums, 0, nums.len());
+    for i in 0..nums.len() {
+        swap_until_smallest(&mut nums[0..=i]);
+    }
 }
 
-// #[test]
-// fn test_quick_sort() {
-//     assert_eq!()
-// }
-
-fn bubble_sort() {
-    todo!();
-}
-
-fn insertion_sort() {
-    todo!();
+#[test]
+fn test_insertion_sort() {
+    let mut arr = [1, 7, 4, 3, 9, 2, 0];
+    insertion_sort(&mut arr);
+    assert_eq!(arr, [0, 1, 2, 3, 4, 7, 9]);
+    let mut arr = [1];
+    insertion_sort(&mut arr);
+    assert_eq!(arr, [1]);
+    let mut arr: [i32; 0] = [];
+    insertion_sort(&mut arr);
+    assert_eq!(arr, []);
 }
 
 fn selection_sort<T: PartialOrd>(nums: &mut [T]) {
@@ -144,13 +173,32 @@ fn selection_sort<T: PartialOrd>(nums: &mut [T]) {
 
 #[test]
 fn test_selection_sort() {
-    let mut arr1 = [1, 7, 4, 3, 9, 2, 0];
-    selection_sort(&mut arr1);
-    assert_eq!(arr1, [0, 1, 2, 3, 4, 7, 9]);
-    let mut arr2 = [1];
-    selection_sort(&mut arr2);
-    assert_eq!(arr2, [1]);
-    let mut arr3: [i32; 0] = [];
-    selection_sort(&mut arr3);
-    assert_eq!(arr3, []);
+    let mut arr = [1, 7, 4, 3, 9, 2, 0];
+    selection_sort(&mut arr);
+    assert_eq!(arr, [0, 1, 2, 3, 4, 7, 9]);
+    let mut arr = [1];
+    selection_sort(&mut arr);
+    assert_eq!(arr, [1]);
+    let mut arr: [i32; 0] = [];
+    selection_sort(&mut arr);
+    assert_eq!(arr, []);
 }
+
+fn quick_sort<T: PartialOrd>(nums: &mut [T]) {
+    fn partition<T: PartialOrd>(nums: &mut [T], low: usize, high: usize) -> usize {
+        todo!()
+    }
+
+    fn _quick_sort<T: PartialOrd>(nums: &mut [T], low: usize, high: usize) {
+        if (low < high) {
+            let pivot_index = partition(nums, low, high);
+            _quick_sort(nums, low, pivot_index);
+            _quick_sort(nums, pivot_index, high);
+        }
+    }
+
+    _quick_sort(nums, 0, nums.len());
+}
+
+#[test]
+fn test_quick_sort() {}
